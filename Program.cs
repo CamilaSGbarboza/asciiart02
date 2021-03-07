316ca6f90606772fab0acda5af8faca5a10a8925
camila <camilasangobar@gmail.com>
2021-03-07 15:43:00 -0300

    lulamolusco

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace asciiart02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("       .--'''''''''--.    ");
+            Console.WriteLine("    .'      .---.      '.    ");
+            Console.WriteLine("   /    .-----------.    \\     ");
+            Console.WriteLine("  /        .-----.        \\    ");
+            Console.WriteLine("  |       .-.   .-.       || ");
+            Console.WriteLine("  |      /   \\ /   \\      || ");
+            Console.WriteLine("  \\     | .-. | .-. ||   // ");
+            Console.WriteLine("    '-._| | | | | | ||.-'     ");
+            Console.WriteLine("        | '-' | '-' ||       ");
+            Console.WriteLine("         \\___/\\___//  ");
+            Console.WriteLine("     _.-'  /    \\  `-._    ");
+            Console.WriteLine("   .' _.--|     ||--._ '.     ");
+            Console.WriteLine("   ' _...-|     ||-..._ '     ");
+            Console.WriteLine("          |     ||          ");
+            Console.WriteLine("          '.___.'         ");
+            Console.ResetColor();
+            
+            
+        }
+    }
+}
